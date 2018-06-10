7fd854a7a07906f93fa8483d683ea8cbe793beca
Won Hong <whong@outlook.com>
2018-06-09 20:29:10 -0400

    updates principle section

--- src/main/react/components/home.jsx
+++ src/main/react/components/home.jsx
@@ -21,6 +21,27 @@ export default class Home extends React.Component {
                src="https://firebasestorage.googleapis.com/v0/b/won-jun-hong-website.appspot.com/o/landpagePhotoHigerRes.jpg?alt=media&token=91348cd2-c278-4bd4-b801-a04f4fe1ed29"/>
         </div>
 
+        <div className="home-intro-content">
+          About:<br/>
+          I currently work as a Tech Consultant at <a href="https://www.kpmg.com"><u>KPMG</u></a>,
+          advising financial institutions with their technology architecture, design, and management strategy.
+          See my previous work experience on <a href="https://www.linkedin.com/in/wjhong/"><u>LinkedIn</u></a> Outside of work,
+          I have a <a href="https://www.linkedin.com/in/ece-ozalp/"><u>lovely partner</u></a> of life, who is an excellent software engineer.
+          Over last number of years, I worked on various <a href="https://wonjunhong.com/portfolio"><u>side projects</u></a>.
+          I am care about making healthcare and finance industries more accessible. Don’t hesitate to get in touch.
+          I would love to have coffee with you.
+        </div>
+        <div className="home-intro-content">
+          Have a project in mind?
+          <div className="contact-buttons">
+            {/*<a class="twitter-follow-button"*/}
+               {/*href="https://twitter.com/towonhong"*/}
+               {/*data-size="large">*/}
+              {/*Follow @WJHong</a>*/}
+
+              {/*Here should go email, twitter, linkedin buttons*/}
+          </div>
+        </div>
       </div>
     )
   }
--- src/main/react/components/principles.jsx
+++ src/main/react/components/principles.jsx
@@ -1,152 +1,213 @@
 import React from "react";
+import Library from "./library";
 
 export default class Principles extends React.Component {
   render() {
     return (
       <div className="principles-container">
         <div className="principles-container-web">
-          <div className="principles-header">
-            Why Principles?
-          </div>
+          {/*<div className="principles-header">*/}
+            {/*Why Principles?*/}
+          {/*</div>*/}
           <div className="principles-content">
-            <p>I see our lives as an endless journey of machine learning. It is a trial-and-error.</p>
+            {/*<p>I see our lives as an endless journey of machine learning. It is a trial-and-error.</p>*/}
 
-            <p>Our brain is a complex set of neurological algorithms. We haven't yet fully understood how it works.
-            But we can get closer by establishing an artificial order and tweak them until we find out the best combo.
-            The order that we are establishing here is our own principles.</p>
+            {/*<p>Our brain is a complex set of neurological algorithms. We haven't yet fully understood how it works.*/}
+            {/*But we can get closer by establishing an artificial order and tweak them until we find out the best combo.*/}
+            {/*The order that we are establishing here is our own principles.</p>*/}
 
-            <p>Actions shape our lives. That’s why we need principles that govern our actions.
-            Whether we know them or not, our actions are often made through intrinsic principles within our brain.
-            After a certain amount of time, if we try to put missing puzzles together, we will soon see the input/output patterns.
-            If we see a pattern, we can tweak our own algorithms. Fix some rules, rid of some, and introduce new ones.
-            For me, this is an interesting journey of life.</p>
+            {/*<p>Actions shape our lives. That’s why we need principles that govern our actions.*/}
+            {/*Whether we know them or not, our actions are often made through intrinsic principles within our brain.*/}
+            {/*After a certain amount of time, if we try to put missing puzzles together, we will soon see the input/output patterns.*/}
+            {/*If we see a pattern, we can tweak our own algorithms. Fix some rules, rid of some, and introduce new ones.*/}
+            {/*For me, this is an interesting journey of life.</p>*/}
 
-            <p>For that, we need our own principles. Not having a set of principles is worse than having a bad set.
-            Bad principles can be fixed, but no principles cannot be fixed.
-              More importantly, we need to discover, establish, execute, reflect, and improve our own principles.
-            If we don’t execute, we will never know which ones work and which ones do not.</p>
+            {/*<p>For that, we need our own principles. Not having a set of principles is worse than having a bad set.*/}
+            {/*Bad principles can be fixed, but no principles cannot be fixed.*/}
+              {/*More importantly, we need to discover, establish, execute, reflect, and improve our own principles.*/}
+            {/*If we don’t execute, we will never know which ones work and which ones do not.</p>*/}
 
-            <p>It is hard to execute our principles since (1) we are governed by our own inertia (or laziness)
-            and (2) we don’t always have enough confidence on our own principles.
-            These barriers often result in us not having a governing attitude over our lives.
-            Some end up spending decades without actually trying to understand what their algorithm is.
-            That’s what I am really afraid of.</p>
+            {/*<p>It is hard to execute our principles since (1) we are governed by our own inertia (or laziness)*/}
+            {/*and (2) we don’t always have enough confidence on our own principles.*/}
+            {/*These barriers often result in us not having a governing attitude over our lives.*/}
+            {/*Some end up spending decades without actually trying to understand what their algorithm is.*/}
+            {/*That’s what I am really afraid of.</p>*/}
 
-            <p>Below are the rules that I have been operating with for last six months,
-            and it seems to be working well so far. However, as I move forward with my machine-learning process,
-            I will tweak, remove, and may even introduce principles that contradicts the ones below.</p>
+            {/*<p>Below are the rules that I have been operating with for last six months,*/}
+            {/*and it seems to be working well so far. However, as I move forward with my machine-learning process,*/}
+            {/*I will tweak, remove, and may even introduce principles that contradicts the ones below.</p>*/}
 
-            <i>Again, this is for reference purpose only.</i>
+            These are my principles; these can change at any time.
           </div>
 
-          <div className="principles-header">
-            Principles
-          </div>
+          {/*<div className="principles-header">*/}
+            {/*Principles*/}
+          {/*</div>*/}
 
           <div className="principles-list">
             <p className="principles-header">Before Going to Bed</p>
-            <p>1. Unless emergency, I go to bed by 10PM.</p>
+            <p>1. Go to bed by 10PM.</p>
             <p className="principles-sub-explanation-indent">
-              I can read paper books, GQ, Vogue, or anything on paper, but never smartphones.
+              I can read GQ, Vogue, Inc. The Economist but not smartphones.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>If something needs to be done ASAP, do it at 5AM next morning.<br/>
+                The result will be better.</i>
             </p>
             <p>
-              2. Revisit today and plan your next day in bed.
+              2. Revisit and plan
             </p>
             <p className="principles-sub-explanation-indent">
-              Think and reproduce key moments of the day. Try to revisit and think what I could have done differently.
-              Leave a mental imprint.
-            </p>
-            <p className="principles-header">Socializing</p>
-            <p>
-              3. Never, ever, hang out with boring, complaining, and low-energy people.
+              While you are in your bed, reproduce key moments of the day.<br/>
             </p>
             <p className="principles-sub-explanation-indent">
-              It is actually better to Netflix than hanging out with these people.
-              Your energy is limited, and these people actually consume your energy a lot more than you think.
-              You are not their psychiatrist.<br/>
-              <i>If someone is truly special, then you need to stand with them. It is important to be loyal to someone who you value.</i>
-            </p>
-            <p>
-              4. Never complain or talk about matters that you have no control.
-            </p>
-            <p className="principles-sub-explanation-indent">
-              Don’t ever complain unless you have a solution <i> unless </i>
-              you are looking for a way to resolve this problem.<br/>
-              <i>Trump is not going to change his plans because you complain at a bar.</i>
-            </p>
-            <p>
-              5. Avoid boring people x 2<br/>
-            </p>
-            <p className="principles-sub-explanation-indent">
-              If your friend does not talk about something new or interesting in four weeks of span, this is a boring one.
-              Write her off. But be gentle when you write someone off.<br/>
-              <i>You are going to spend six months months with your family at the Amalfi Coast
-                would be a harmless excuse.</i>
-            </p>
-            <p className="principles-header">Communication</p>
-            <p>
-              6. KISS--keep it simple and stupid.
-            </p>
-            <p className="principles-sub-explanation-indent">
-              Richard Feynman has been popular since he explained quantum physics in layman's language.<br/>
-              <i>Stupid but works.</i>
-            </p>
-            <p>
-              7. Never talk about your job or your family.
-            </p>
-            <p className="principles-sub-explanation-indent">
-              Who wants to hear whether your git push failed. Think of three exciting things as you brew your morning coffee.<br/>
-              <i>Think hard to entertain people. Excite them. Energize them.</i>
+              <i>Try to revisit and think what I could have done differently.</i>
             </p>
             <p className="principles-header">
               Facing Difficulties
             </p>
             <p>
-              8. If there’s a will, there is a solution.
+              3. You have a problem? Now is time to solve.
             </p>
             <p className="principles-sub-explanation-indent">
-              There are always ways to solve problems. What lacks is my perseverance and tolerance.
+              Don't shy away from the problems because they are painful.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>If you don't solve the problem now, it will grow at a compounding interest rate.</i>
             </p>
             <p>
-              9. Close to street smart people. You will soon be one.
+              4. If there’s a will, there is a solution.
             </p>
             <p className="principles-sub-explanation-indent">
-              Hang out with street smarts and take a good care of them. They have solutions for everything, from breaking your contract to negotiating your contract.
+              There are always ways to solve problems.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>What lacks is my perseverance and tolerance.</i>
+            </p>
+            <p>
+              5. Stay close with the streetsmarts.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              Hang out with street smarts and take a good care of them.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Soon, you will become one. Streetsmarts have solutions for everything.</i>
+            </p>
+            <p>
+              6. Deliver on Deadline.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              It is important to get things done on-time and meet the deadline.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Nothing is more cliche than perfect products that missed the timing.</i>
             </p>
             <p className="principles-header">When you are Hesitant</p>
             <p>
-              10. When you are reluctant, ask yourself, “what do I have to lose?”
+              7. If reluctant, ask yourself, “what do I have to lose?”
             </p>
             <p className="principles-sub-explanation-indent">
-              If it is something trivial like, your personal pride, time, or effort, then do it. What do you really have to lose?
+              If something needs to be done, do it now, rather than later.<br/>
             </p>
             <p className="principles-sub-explanation-indent">
-              <i>Believe me, I've heard so many regrets of my friends not talking to the cute girl sitting at a cafe,
-                but I haven’t heard the opposite.</i>
+              <i>Believe me, I've heard plenty of regrets of my friends not talking to the cute girl sitting at a cafe,
+                but I haven’t heard the opposite yet.</i>
+            </p>
+            <p>
+              8. Momentum is a key
+            </p>
+            <p className="principles-sub-explanation-indent">
+              If it starts moving, you need to push it all the way and close it.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Kinetic Energy = 1/2*m*v&#178;. It's physics.<br/>
+                The moment you lose the velocity, energy goes down "exponentially."</i>
+            </p>
+            <p className="principles-header">Communication</p>
+            <p>
+              9. Keep it simple and stupid (KISS)
+            </p>
+            <p className="principles-sub-explanation-indent">
+              Don't be a New Yorker Writer or Malcolm Gladwell.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Richard Feynman and Donald Trump work better in real world.</i>
+            </p>
+            <p>
+              10. Never talk about your job or family.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              Who wants to hear whether your git push failed.<br/>
+              Think of three exciting things as you brew your morning coffee.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Think hard to entertain people. Excite them. Energize them.</i>
+            </p>
+            <p>
+              11. Never talk about things that you can't control.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              Don’t ever present a problem <i> unless </i>
+              you are trying to resolve them.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Trump is <i>not</i> going to change his plans because you complain.</i>
+            </p>
+            <p>
+              12. Don't get into arguments.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              There are people like Carl Icahn, Donald Trump, and Travis Kalanick.<br/>
+              They will shoot their foot with a machine gun just to win an argument.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Say "you are right" and move on. Maybe I am wrong.</i>
             </p>
             <p className="principles-header">
               People Relations
             </p>
             <p>
-              11. Focus on people who you care.
+              13. Only focus on who you care.
             </p>
             <p className="principles-sub-explanation-indent">
-              I would love to be a gentleman to everyone, but that's hard since our resources, time, and energy are limited.
-              We just have to focus on people that we love and care about.
               Always, always, always remember your family’s birthday, Valentine’s day, best friend’s birthday.
-              Surprise them. It is okay to be crazy. Be innovative and become a memorable one.
+
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>Surprise them. It is okay to be crazy. Be innovative and become a memorable one.</i>
             </p>
             <p>
-              12. Be nice to everyone if you have to spend time with them.
+              14. Be nice to everyone if you have to spend time with them.
             </p>
             <p className="principles-sub-explanation-indent">
               There are many definitions of being smart and successful.
               For me, being successful or smart means being a go-getter
               while entertaining and energizing people around you.
-              At times, stressful moments take place. That's just part of life.
-              But as long as you are entertaining and energizing 80% of the time,
-              I think people will remember you as a good person. <br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
               <i>That's the ambitious goal that I am striving for.</i>
+            </p>
+            <p>
+              15. Never, ever, hang out with boring and low-energy people--especially complainers.
+            </p>
+            <p className="principles-sub-explanation-indent">
+              These people are carcinogens of our society. Complainers are addictive and contagious.
+              Your energy is limited, and these people will drain your energy. You are not their psychiatrist.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>If someone is truly special, then you need to stand with them.<br/>
+                It is important to be loyal to someone who you value.</i>
+            </p>
+            <p>
+              16. Avoid boring people x 2<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              If someone does <i>not</i> say something new or interesting in four weeks of span, write her off.
+              But be gentle when you write someone off.<br/>
+            </p>
+            <p className="principles-sub-explanation-indent">
+              <i>You are going to spend six months months with your family at the Amalfi Coast
+                would be a harmless excuse.</i>
             </p>
           </div>
         </div>
